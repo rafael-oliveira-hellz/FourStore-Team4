3ec2919849eecd937ba739ad5f8d5ecf16271b6e
Pedro Augusto <pedro4032@hotmail.com>
2022-05-20 09:18:35 -0300

    Create HashMap in Transaction model

--- src/br/com/foursys/fourcamp/fourstore/model/Transaction.java
+++ src/br/com/foursys/fourcamp/fourstore/model/Transaction.java
@@ -1,44 +1,44 @@
 package br.com.foursys.fourcamp.fourstore.model;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Transaction {
-	private ArrayList<Integer> quantityProduct;
-	private ArrayList<Product> products;
+	private HashMap<Product, Integer> products;
 	private Costumer constumer;
 	private Double totalPrice;
 	
 	public Transaction(Costumer constumer) {
 		this.constumer = constumer;
 		this.totalPrice = 0.0;
-		quantityProduct = new ArrayList<Integer>();
-		products = new ArrayList<Product>();
+		products = new HashMap<Product, Integer>();
 	}
 
 	public Costumer getConstumer() {
 		return constumer;
 	}
 
-	public Double getTotalPrice() {
-		int qtt = 0;
-		double price = 0.0;
-		
-		for(int i = 0; i < quantityProduct.size(); i++) {
-			qtt = quantityProduct.get(i);
-			price = products.get(i).getSellPrice();
-			totalPrice +=price * qtt;
-		}
+	public HashMap<Product, Integer> getProducts() {
+		return products;
+	}
+
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public Double getTotalPrice() {		
+		products.forEach((key, value) -> {
+			totalPrice += key.getBuyPrice() * value;			
+		});
 		return totalPrice;
 	}
 
 	public void addProducts(Product product, Integer quantityProduct) {
-		this.products.add(product);
-		this.quantityProduct.add(quantityProduct);
+		this.products.put(product, quantityProduct);
 	}
 
 	@Override
 	public String toString() {
-		return "Transaction [quantityProduct=" + quantityProduct + ", products=" + products + ", constumer=" + constumer + ", totalPrice="
+		return "Transaction [products=" + products.toString() + ", constumer=" + constumer + ", totalPrice="
 				+ totalPrice + "]";
 	}
 	

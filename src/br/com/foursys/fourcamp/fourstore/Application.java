c1630e9c890b12d40eec143f18693b5c5ce0195d
Diogo Valente <diogovalente@hotmail.com.br>
2022-05-26 14:57:37 -0300

    bug fixing

--- src/br/com/foursys/fourcamp/fourstore/Application.java
+++ src/br/com/foursys/fourcamp/fourstore/Application.java
@@ -6,6 +6,7 @@ package br.com.foursys.fourcamp.fourstore;
 import br.com.foursys.fourcamp.fourstore.communication.MainMenu;
 import br.com.foursys.fourcamp.fourstore.exception.InvalidSellValueException;
 import br.com.foursys.fourcamp.fourstore.exception.ProductNotFoundException;
+import br.com.foursys.fourcamp.fourstore.exception.StockInsufficientException;
 
 /**
  * @author Rafael de Oliveira
@@ -23,12 +24,13 @@ public class Application {
 	 * @param args
 	 * @throws ProductNotFoundException 
 	 * @throws InvalidSellValueException 
+	 * @throws StockInsufficientException 
 	 */
-	public static void main(String[] args) throws InvalidSellValueException, ProductNotFoundException {
+	public static void main(String[] args) throws InvalidSellValueException, ProductNotFoundException, StockInsufficientException {
 
 		MainMenu menu = new MainMenu();
 
-		menu.login();
+		
 	}
 
 }
--- src/br/com/foursys/fourcamp/fourstore/communication/StockMenuCommunication.java
+++ src/br/com/foursys/fourcamp/fourstore/communication/StockMenuCommunication.java
@@ -233,6 +233,6 @@ public class StockMenuCommunication {
 	}
 
 	public static void updateProduct() {
-
+		
 	}
 }
